6d2a70bb7e062a043eeade36454a17d605e26223
¨anniezh12¨ <aniqaelahi@yahoo.com>
2018-01-18 19:50:42 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 // Code goes here
+import React from 'react';
+import  ReactDom from 'react-dom';
+
+//       const title = React.createElement('h1',{},
+//        'My First React Code');
+//
+//       const paragraph = React.createElement('p',{},
+//       'Writing more HTML!');
+//
+// const container = React.createElement('div',  {},
+// [title,paragraph]);
+//
+//
+//       ReactDom.render(
+//         container,
+//         document.getElementById('global')
+//       );
+
+
+
+const list = React.createElement('div',{},
+                React.createElement('h1',{},'My favorite ice cream flavor'),
+                React.createElement('ul',{},
+                  [React.createElement('li',{className:'brown'}, 'Chocolate'),
+                   React.createElement('li',{className:'white'}, 'Vanilla'),
+                    React.createElement('li',{className:'yellow'}, 'Banana')
+                  ]
+                )
+              );
+
+              ReactDom.render(
+                list,
+                document.getElementById('global')
+              );
